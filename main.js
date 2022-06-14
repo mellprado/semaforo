7555a8d9bd781dc047fb002af89081cc2168b2ba
Melissa <107324010+mellprado@users.noreply.github.com>
2022-06-14 01:07:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,25 +2,38 @@ const redLight = document.getElementById("redLight");
 const yellowLight = document.getElementById("yellowLight");
 const greenLight = document.getElementById("greenLight");
 const turnOff = document.getElementById("turnOff");
+const auto = document.getElementById("auto");
 const semaphore = document.getElementById("semaphore");
+const colors = ["green", "yellow", "red"];
 
-function redLightOn() {
-    semaphore.src = "./img/vermelho.png";
+var colorIndex = 0;
+
+const light = {
+    "red": () => {semaphore.src = "./img/vermelho.png";},
+    "yellow": () => {semaphore.src = "./img/amarelo.png";},
+    "green": () => {semaphore.src = "./img/verde.png";},
+    "off": () => {semaphore.src = "./img/desligado.png";}
+};
+
+function automatic() {
+    setInterval(changeLight, 2000)
 }
 
-function yellowLightOn() {
-    semaphore.src = "./img/amarelo.png";
+function changeLight() {
+    const color = colors[colorIndex];
+    light[color]();
+    nextIndex();
 }
 
-function greenLightOn() {
-    semaphore.src = "./img/verde.png";
+function nextIndex() {
+    colorIndex++;
+    if (colorIndex >= colors.length) {
+        colorIndex = 0;
+    }
 }
 
-function semaphoreOff() {
-    semaphore.src = "./img/desligado.png";
-}
-
-redLight.addEventListener("click", redLightOn);
-yellowLight.addEventListener("click", yellowLightOn);
-greenLight.addEventListener("click", greenLightOn);
-turnOff.addEventListener("click", semaphoreOff);
+redLight.addEventListener("click", light.red);
+yellowLight.addEventListener("click", light.yellow);
+greenLight.addEventListener("click", light.green);
+turnOff.addEventListener("click", light.off);
+auto.addEventListener("click", automatic);
